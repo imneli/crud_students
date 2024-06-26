3e54964364a1484989e3a5cdad190e4c50b598f1
imneli <contatodoneli@gmail.com>
2024-06-26 10:46:45 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -11,7 +11,7 @@ module.exports = db;
 
 require('dotenv').config();
 
-// modelo: 
+// modelo:
 
 // module.exports = {
 //   dialect: 'mariadb',
@@ -34,3 +34,32 @@ require('dotenv').config();
 //   timezone: 'America/Sao_Paulo'
 
 // }
+
+require('dotenv').config();
+
+module.exports = {
+  /* SQLite */
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+
+  /* MySQL / MariaDB */
+  // host: process.env.DATABASE_HOST,
+  // port: process.env.DATABASE_PORT,
+  // username: process.env.DATABASE_USERNAME,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE,
+  // dialectOptions: {
+  //   timezone: 'America/Sao_Paulo',
+  // },
+  // timezone: 'America/Sao_Paulo',
+
+  /* ALL */
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+};
+
